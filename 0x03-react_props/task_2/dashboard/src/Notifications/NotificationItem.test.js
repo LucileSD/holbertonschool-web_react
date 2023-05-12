8b17ec42d6c7515a9ca81bd4b516c7668bb0a6ff
LucileSD <4316@holbertonschool.com>
2023-05-12 14:02:14 +0200

    ;

--- 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import NotificationsItem from './NotificationItem';
+import NotificationItem from './NotificationItem';
 
 describe('<NotificationItem />', () => {
   it('renders without crashing', () => {
-    shallow(<NotificationsItem />);
+    shallow(<NotificationItem />);
   });
 
   it('should render <li data-priority="default">test</li>', () => {
-    const wrapper = shallow(<NotificationsItem type="default" value="test" />);
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
     expect(wrapper.contains(<li data-priority="default">test</li>)).toEqual(true);
-  })
+  });
 
   it('should render ', () => {
-    const wrapper = shallow(<NotificationsItem type="default" html={{ __html: '<u>test</u>' }} />);
+    const wrapper = shallow(<NotificationItem type="default" html={{ __html: '<u>test</u>' }} />);
     expect(wrapper.html()).toEqual('<li data-priority="default"><u>test</u></li>');
-  })
+  });
 })
