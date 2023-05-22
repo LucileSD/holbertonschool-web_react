cd5ed355070c3ffb99c648f87ebfc662438dd0c8
LucileSD <4316@holbertonschool.com>
2023-05-22 18:51:31 +0200

    markAsRead test

--- 0x04-React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04-React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -18,4 +18,16 @@ describe('<NotificationItem />', () => {
     const wrapper = shallow(<NotificationItem type="default" html={{ __html: '<u>test</u>' }} />);
     expect(wrapper.html()).toEqual('<li data-priority="default"><u>test</u></li>');
   });
+
+  it('should log to the console a string with appropriate id', () => {
+    const id = 1;
+    const wrapper = shallow(<NotificationItem type='default' value='test' id={id}/>);
+    const instance = wrapper.instance();
+    instance.markAsRead = jest.fn();
+    const listItem = wrapper.find('li').first();
+    listItem.simulate('click');
+    instance.markAsRead(id);
+    expect(instance.markAsRead).toHaveBeenCalledWith(1);
+    jest.restoreAllMocks();
+  })
 })
