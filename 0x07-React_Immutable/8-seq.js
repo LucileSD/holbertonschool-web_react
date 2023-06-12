0d85a5b43efd8d1ca6bef6ae430c4abda662ba8b
LucileSD <4316@holbertonschool.com>
2023-06-12 15:01:20 +0200

    Lazy Seq

---
+++ 0x07-React_Immutable/8-seq.js
@@ -0,0 +1,17 @@
+import { Seq } from 'immutable';
+
+export function printBestStudents(grades) {
+  const newSequence = Seq(grades)
+    .filter((x) => {
+      return x.score > 70
+    });
+
+  const newObj = newSequence.toJS();
+
+  Object.keys(newObj).map(item => {
+    newObj[item].firstName = newObj[item].firstName.charAt(0).toUpperCase() + newObj[item].firstName.slice(1);
+    newObj[item].lastName = newObj[item].lastName.charAt(0).toUpperCase() + newObj[item].lastName.slice(1);
+  })
+
+  console.log(newObj);
+}
