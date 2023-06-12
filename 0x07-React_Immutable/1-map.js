07943b51396ac3c7a6674e5824b01412505b8917
LucileSD <4316@holbertonschool.com>
2023-06-12 11:01:24 +0200

    without exemple

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -5,12 +5,3 @@ function getImmutableObject(object) {
 }
 
 export default getImmutableObject;
-
-const exObj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-}
-const exemple = getImmutableObject(exObj);
-console.log(exemple);
