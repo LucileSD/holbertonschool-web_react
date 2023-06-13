bc3a991aaca6fe199439e4c9759a341dfcf0f59d
LucileSD <4316@holbertonschool.com>
2023-06-13 10:23:20 +0200

    map after filter

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -2,14 +2,14 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
   const capitalize = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
   const newSequence = Seq(object)
     .filter(x => x.score > 70)
     .map((item) => {
-      const { firstName, lastName } = item;
       return {
         ...item,
-        firstName: capitalize(firstName),
-        lastName: capitalize(lastName),
+        firstName: capitalize(item.firstName),
+        lastName: capitalize(item.lastName),
       };
     }).toJS();
 
